ce960a80032dc69111e5de519a279d48c53d2d70
Benji Fisher <bfisher@isovera.com>
2017-07-14 14:01:05 -0400

    Use the composer-generated autoloader for the HTML5 parser
    Remove the lines that explicitly load files from vendor/Masterminds.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -41,5 +41,4 @@ require_once dirname( __DIR__ ) . '/php-typography-autoload.php';
 /**
  * Load HTML parser for function testing.
  */
-require_once dirname( __DIR__ ) . '/vendor/Masterminds/HTML5.php';
-require_once dirname( __DIR__ ) . '/vendor/Masterminds/HTML5/autoload.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
